656833da0f3860842df661121c13a6c569e70229
Frank S. Hestvik <tristesse@gmail.com>
2025-08-30 20:40:40 +0200

    unigrep improvements & more brackets for keymap.

--- unigrep.py
+++ unigrep.py
@@ -2,10 +2,16 @@
 import csv
 import re
 import sys
+from dataclasses import dataclass
+
 
 if len(sys.argv) <= 1:
   print(f"""
-usage: {sys.argv[0]} [[-][.]<regex> [-][.][regex] ...]
+usage: {sys.argv[0]} [-c] [-n] [-#] [[-][.]<regex> [-][.][regex] ...]
+
+  -c sort by character
+  -n sort by name
+  -# sort by code point (number)
 
 Lists all the Unicode characters whose name is matched
 by (all) the given regular expressions.
@@ -19,6 +25,15 @@ Uses case insensitive matching.
 
 """)
   sys.exit(0)
+
+
+@dataclass
+class Char():
+  chr: str
+  name: str
+  cat: str
+  ord: int
+
 dia = csv.unix_dialect()
 dia.delimiter = ';'
 reader = csv.reader(open('UnicodeData.txt', 'r'), dia)
@@ -26,8 +41,9 @@ reader = csv.reader(open('UnicodeData.txt', 'r'), dia)
 headers = 'Code_Point;Name;General_Category;Canonical_Combining_Class;Bidi_Class;Decomposition_Type_and_Decomposition_Mapping;Numeric_Type;Numeric_Value_for_Type_Digit;Numeric_Value_for_Type_Numeric;Bidi_Mirrored;Unicode_1_Name;ISO_Comment;Simple_Uppercase_Mapping;Simple_Lowercase_Mapping;Simple_Titlecase_Mapping'.split(';')
 fields = { name.lower(): i for i, name in enumerate(headers) }
 
-cnames = { chr(int(c, 16)): name for (c, name, *_) in reader }
-nnames = { v: k for k, v in cnames.items() }
+cnames = [ Char(chr(int(c, 16)), name, cat, int(c, 16))
+          for (c, name, cat, *_) in reader ]
+nnames = { c.name: c for c in cnames }
 
 def grep(*regexes):
   ms = [re.compile(x, re.I) for x in regexes]
@@ -36,20 +52,43 @@ def grep(*regexes):
       yield v
 
 left = list(nnames.keys())
+sort = None
+
 for arg in sys.argv[1:]:
+  if arg == '-n':
+    sort = 'name'
+    continue
+  if arg == '-c':
+    sort = 'chr'
+    continue
+  if arg == '-#':
+    sort = 'ord'
+    continue
   invert = False
-  if arg[:1] in '-!':
-    invert = True
+  if '>' in arg:
+    l, r = arg.upper().split('>', maxsplit=1)
+    ls = set((x, x.replace(l, r)) for x in left if l in x)
+    ls = [(x, y) for x, y in ls if y in nnames]
+    ls, rs = zip(*ls)
+    left = ls + rs
+  elif arg.startswith(':'):
     arg = arg[1:]
-  if arg[:1] in '.':
-    arg = fr'\b{arg[1:]}\b'
-  regex = re.compile(arg, re.I)
-  left = [x for x in left if bool(regex.search(x)) ^ invert]
+    left = [x for x in left if nnames[x].cat.startswith(arg)]
+  else:
+    if arg[:1] in '-!':
+      invert = True
+      arg = arg[1:]
+    if arg[:1] in '.':
+      arg = fr'\b{arg[1:]}\b'
+    regex = re.compile(arg, re.I)
+    left = [x for x in left if bool(regex.search(x)) ^ invert]
   if not left:
     print('no matches!')
     break
 
+if sort is not None:
+  left = sorted(left, key=lambda n: getattr(nnames[n], sort))
 for k in left:
-  print(nnames[k], k)
+  print(nnames[k].chr, nnames[k].cat, k)
 
 
